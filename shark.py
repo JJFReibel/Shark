565b20b5c6948718573ff6bf6a7ed38b5c1d0253
Jean-Jacques François Reibel <9219877+JJFReibel@users.noreply.github.com>
2021-09-04 14:12:19 -0700

    Uploaded shark.py

---
+++ shark.py
@@ -0,0 +1,14 @@
+# By Jean-Jacques F. Reibel
+# Shark or Barracuda
+# I will not be held responsible for:
+# any shenanigans
+# ಠ_ಠ
+# ¯¯\_(ツ)_/¯¯
+import os
+
+os.system("printf '\e[0;34;1;1m\n           |'")
+os.system("printf '\e[0;34;1;1m\n          /|      /|\n'")
+os.system("printf '\e[0;31;1;1m     >'")
+os.system("printf '\e[0;33;1;5m°'")
+os.system("printf '\e[0;34;1;1m) )))-----< |\n'")
+os.system("printf '\e[0;34;1;1m         \\|       \\|\n\n'")
